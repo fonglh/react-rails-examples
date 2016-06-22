f6132b75ce25336da25b770b43ee5a28ee11af7a
fonglh <fonglh@gmail.com>
2016-06-22 16:35:42 +0800

    Modify state with textbox.

--- app/assets/javascripts/components/basic.es6.jsx
+++ app/assets/javascripts/components/basic.es6.jsx
@@ -5,8 +5,14 @@ class Basic extends React.Component {
     this.state = { name: 'Vipul' };
   }
 
+  handleTextChange(e) {
+    this.setState({name: e.target.value});
+  }
+
   render() {
-    return (<div>
+    return (
+      <div>
+      <input type="text" onChange={this.handleTextChange.bind(this)} />
       <h1>Hello, {this.props.name}</h1>
       <h1>My name is, {this.state.name}</h1>
     </div>);
